efe0ad3cd51b6ae616a6c89d996f8bf647f2ed4e
wutian <774727551@qq.com>
2017-06-08 15:24:28 +0800

    article completed

--- js/article.js
+++ js/article.js
@@ -3,4 +3,9 @@ $(function(){
     $("#aside-btn").on("tap",function(){
         mui('.mui-off-canvas-wrap').offCanvas('toggle');
     });
+
+    /*文章区域滚动*/
+    mui('.mui-scroll-wrapper').scroll({
+        deceleration: 0.0005
+    });
 });
